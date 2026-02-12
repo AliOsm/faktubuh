a37bfa4a45c723f246a604177ae28f6a2facb23b
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2026-02-12 09:12:34 +0300

    Enable always_include_errors_hash inertia config

--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -4,4 +4,5 @@ InertiaRails.configure do |config|
   config.version = ViteRuby.digest
   config.use_script_element_for_initial_page = true
   config.parent_controller = "::InertiaController"
+  config.always_include_errors_hash = true
 end
